d8b13f63cab351bc1483670ee24e1b5a75e32afd
Lanseria <zhangchao564265135@hotmail.com>
2017-04-16 11:15:28 +0800

    更新了validate的jqeury的插件,应该说添加了

--- public/js/oninput.js
+++ public/js/oninput.js
@@ -2,9 +2,19 @@ $('#progress').bind('input propertychange', function(){
   var pro = $(this).val();
   if (pro>100||pro<0) {
     alert("数值为0-100");
+    $(this).val('100');
+    pro = 100;
   }
   // $('#thisprogress').attr('aria-valuenow') = ""+pro;
   //style="width: 60%;min-width: 2em;"
   $('#thisprogress').css("width", pro+"%");
   $('#thisprogress').html(pro+"%");
 })
+$.validator.setDefaults({
+    submitHandler: function() {
+      alert("提交了哦!");
+    }
+});
+$().ready(function() {
+    $("#commentForm").validate();
+});
